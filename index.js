8a5bd14224c5c1e9504c9097822298983fb22551
smabari <mdazizulbarishafin@gmail.com>
2025-05-21 13:36:17 +0600

    connected mongodb

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// user: smabari
+// pass: MxTKpLICVPAtYa0Q
+const uri =
+  "mongodb+srv://smabari:MxTKpLICVPAtYa0Q@cluster0.bsqinhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (request, response) => {
+  response.send("crud running");
+});
+
+app.listen(port, () => {
+  console.log(`crud server running in ${port}`);
+});
